32ccfb6b886c4d05c23a578f3ec10ba921ce8d5d
tangmeng <357003671@qq.com>
2017-03-09 14:02:52 +0800

    add auto fontsize

---
+++ _site/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.gif-img').each(function(){
+    var $el = $(this)
+    var src = $el.find('img').attr('src');
+    $el.hover(
+      function(){
+        $(this).find('img').attr("src", src.replace('.png', '.gif'));
+      },
+      function(){
+        $(this).find('img').attr("src", src);
+      });
+  });
+  new WOW().init();
+  $('#test').popover();
+  $('.carousel').carousel()
+});
